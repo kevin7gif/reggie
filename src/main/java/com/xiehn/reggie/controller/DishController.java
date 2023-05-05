73ee1596c3ae3866edae93e1fa6c027cbf0683f8
kevin7gif <2632176642@qq.com>
2023-05-05 16:52:32 +0800

    使用redis缓存短信验证码和菜品数据

--- src/main/java/com/xiehn/reggie/controller/DishController.java
+++ src/main/java/com/xiehn/reggie/controller/DishController.java
@@ -15,10 +15,12 @@ import com.xiehn.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -34,6 +36,8 @@ public class DishController {
     private DishFlavorService dishFlavorService;
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 新增菜品
@@ -43,6 +47,11 @@ public class DishController {
     @PostMapping
     public R<String> save(@RequestBody DishDto dishDto){//当需要向前端传送json数据时，加上此注解
         log.info(dishDto.toString());
+
+        //清理修改的分类里面的菜品缓存数据
+        String key="dish_"+dishDto.getCategoryId()+"_1";
+        redisTemplate.delete(key);
+
         dishService.saveWithFlavor(dishDto);
 
         return R.success("新增菜品成功！");
@@ -125,7 +134,12 @@ public class DishController {
         log.info(dishDto.toString());
 
         dishService.updateWithFlavor(dishDto);
-        return R.success("新增菜品成功！");
+
+        //清理修改的分类里面的菜品缓存数据
+        String key="dish_"+dishDto.getCategoryId()+"_1";
+        redisTemplate.delete(key);
+
+        return R.success("修改菜品成功！");
     }
 
     /**
@@ -140,6 +154,16 @@ public class DishController {
         LambdaUpdateWrapper<Dish> updateWrapper = new LambdaUpdateWrapper<>();
         updateWrapper.in(ids != null, Dish::getId, ids);
         updateWrapper.set(Dish::getStatus, status);
+
+        LambdaQueryWrapper<Dish> queryWrapper=new LambdaQueryWrapper<>();
+        queryWrapper.in(ids!=null,Dish::getId,ids);
+        List<Dish> dishList = dishService.list(queryWrapper);
+        for (Dish dish : dishList) {
+            Long categoryId = dish.getCategoryId();
+            String key="dish_"+categoryId+"_1";
+            redisTemplate.delete(key);
+        }
+
         dishService.update(updateWrapper);
         return R.success("批量操作成功");
     }
@@ -198,6 +222,19 @@ public class DishController {
     @GetMapping("/list")
     public R<List<DishDto>> list(Dish dish){
 
+        List<DishDto> dishDtoList=null;
+
+        //动态构造一个带有菜品分类id和菜品状态的key（查询的时候就是通过菜品分类id和菜品状态进行查询的）
+        String key="dish_"+dish.getCategoryId()+"_"+dish.getStatus();//dish_xxxxxxxx_1
+
+        //先从redis中获取缓存数据
+        dishDtoList=(List<DishDto>) redisTemplate.opsForValue().get(key);
+
+        if(dishDtoList!=null){
+            //如果存在，直接返回，无需查询数据库
+            return R.success(dishDtoList);
+        }
+
         //构造查询条件
         LambdaQueryWrapper<Dish> queryWrapper=new LambdaQueryWrapper<>();
         queryWrapper.eq(dish.getCategoryId()!=null,Dish::getCategoryId,dish.getCategoryId());
@@ -209,7 +246,7 @@ public class DishController {
 
         List<Dish> list = dishService.list(queryWrapper);
 
-        List<DishDto> dishDtoList=list.stream().map((item)->{
+        dishDtoList=list.stream().map((item)->{
             DishDto dishDto=new DishDto();
 
             BeanUtils.copyProperties(item,dishDto);
@@ -232,6 +269,9 @@ public class DishController {
             dishDto.setFlavors(dishFlavorList);
             return dishDto;
         }).collect(Collectors.toList());
+
+        //如果不存在，需要查询数据库，将查询到的菜品数据缓存到redis,并设置缓存时间为60分钟
+        redisTemplate.opsForValue().set(key,dishDtoList,60, TimeUnit.MINUTES);
 
         return R.success(dishDtoList);
     }
--- src/main/java/com/xiehn/reggie/controller/UserController.java
+++ src/main/java/com/xiehn/reggie/controller/UserController.java
@@ -4,14 +4,15 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xiehn.reggie.common.R;
 import com.xiehn.reggie.pojo.User;
 import com.xiehn.reggie.service.UserService;
-import com.xiehn.reggie.utils.SMSUtils;
 import com.xiehn.reggie.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -19,6 +20,8 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 移动端手机发送短信验证码
@@ -39,7 +42,10 @@ public class UserController {
             //SMSUtils.sendMessage("瑞吉","SMS_460685320",phone,code);
 
             //将生成的验证码保存到session对象
-            session.setAttribute(phone,code);
+            //session.setAttribute(phone,code);
+
+            //将生成的验证码缓存到redis中，并设置有效期为5分钟
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
 
             return R.success("手机验证码短信发送成功！");
         }
@@ -65,7 +71,10 @@ public class UserController {
         String code = map.get("code").toString();
 
         //从session当中获取保存的验证码
-        Object codeInSession = session.getAttribute(phone);
+        //Object codeInSession = session.getAttribute(phone);
+
+        //从redis中获取缓存的验证码
+        Object codeInSession=redisTemplate.opsForValue().get(phone);
 
         //进行验证码的比对（页面提交的验证码和session当中的验证码）
         if(codeInSession!=null && codeInSession.equals(code)){
@@ -83,6 +92,10 @@ public class UserController {
                 userService.save(user);
             }
             session.setAttribute("user",user.getId());
+
+            //如果用户登录成功，删除redis中缓存的验证码
+            redisTemplate.delete(phone);
+
             return R.success(user);
         }
         return R.error("登录失败！");
