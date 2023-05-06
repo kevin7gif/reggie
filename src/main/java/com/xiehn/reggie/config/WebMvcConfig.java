b57f75300e71f17879fda3366272b36a1531c9f8
kevin7gif <2632176642@qq.com>
2023-05-07 00:23:53 +0800

    使用Swagger来生成接口文档并调试接口

--- src/main/java/com/xiehn/reggie/config/WebMvcConfig.java
+++ src/main/java/com/xiehn/reggie/config/WebMvcConfig.java
@@ -1,18 +1,42 @@
 package com.xiehn.reggie.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.xiehn.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @Slf4j
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+@EnableSwagger2
+@EnableKnife4j
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始进行静态资源映射...");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
     /**
      * 扩展mvc框架的消息转换器
      * @param converters
@@ -26,5 +50,32 @@ public class WebMvcConfig implements WebMvcConfigurer {
         messageConverter.setObjectMapper(new JacksonObjectMapper());
         //将上面的消息转换器对象追加到mvc框架的转换器集合的最前面
         converters.add(0,messageConverter);
+    }
+
+    /**
+     * 生成接口文档
+     * @return
+     */
+    @Bean
+    public Docket createRestApi() {
+        // 文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.xiehn.reggie.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 描述接口文档的信息
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("瑞吉外卖")
+                .version("1.0")
+                .description("瑞吉外卖接口文档")
+                .build();
     }
 }
